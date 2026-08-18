7784de0bf4b76d4321f428814dbccde90ba30ebf
dporrasg-boop <dporrasg@ctpplatanares.ed.cr>
2026-08-18 08:13:09 -0600

    FINAL
    final

--- RA6_flet/Calculadora.py
+++ RA6_flet/Calculadora.py
@@ -1,27 +1,173 @@
 
+from dataclasses import field
 import flet as ft
 
-def main(page: ft.Page):
-    page.title = "Calc App"
-    result = ft.Text(value="0")
-    page.add(
-        import flet as ft
+
+ft.control
+class CalcButton(ft.Button):
+    expand: int = field(default_factory=lambda: 1)
+
+
+ft.control
+class DigitButton(CalcButton):
+    bgcolor: ft.Colors = ft.Colors.WHITE_24
+    color: ft.Colors = ft.Colors.WHITE
+
+
+ft.control
+class ActionButton(CalcButton):
+    bgcolor: ft.Colors = ft.Colors.ORANGE
+    color: ft.Colors = ft.Colors.WHITE
+
+
+ft.control
+class ExtraActionButton(CalcButton):
+    bgcolor: ft.Colors = ft.Colors.BLUE_GREY_100
+    color: ft.Colors = ft.Colors.BLACK
+
+
+ft.control
+class CalculatorApp(ft.Container):
+    def init(self):
+        self.reset()
+        self.width = 350
+        self.bgcolor = ft.Colors.BLACK
+        self.border_radius = ft.BorderRadius.all(20)
+        self.padding = 20
+        self.result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
+
+        self.content = ft.Column(
+            controls=[
+                ft.Row(
+                    controls=[self.result],
+                    alignment=ft.MainAxisAlignment.END,
+                ),
+                ft.Row(
+                    controls=[
+                        ExtraActionButton(content="AC", on_click=self.button_clicked),
+                        ExtraActionButton(content="+/-", on_click=self.button_clicked),
+                        ExtraActionButton(content="%", on_click=self.button_clicked),
+                        ActionButton(content="/", on_click=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(content="7", on_click=self.button_clicked),
+                        DigitButton(content="8", on_click=self.button_clicked),
+                        DigitButton(content="9", on_click=self.button_clicked),
+                        ActionButton(content="*", on_click=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(content="4", on_click=self.button_clicked),
+                        DigitButton(content="5", on_click=self.button_clicked),
+                        DigitButton(content="6", on_click=self.button_clicked),
+                        ActionButton(content="-", on_click=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(content="1", on_click=self.button_clicked),
+                        DigitButton(content="2", on_click=self.button_clicked),
+                        DigitButton(content="3", on_click=self.button_clicked),
+                        ActionButton(content="+", on_click=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(
+                            content="0", expand=2, on_click=self.button_clicked
+                        ),
+                        DigitButton(content=".", on_click=self.button_clicked),
+                        ActionButton(content="=", on_click=self.button_clicked),
+                    ]
+                ),
+            ]
+        )
+
+    def button_clicked(self, e):
+        data = e.control.content
+        print(f"Button clicked with data = {data}")
+        if self.result.value == "Error" or data == "AC":
+            self.result.value = "0"
+            self.reset()
+
+        elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
+            if self.result.value == "0" or self.new_operand:
+                self.result.value = str(data)
+                self.new_operand = False
+            else:
+                self.result.value = str(self.result.value) + str(data)
+
+        elif data in ("+", "-", "*", "/"):
+            self.result.value = str(
+                self.calculate(
+                    self.operand1, float(self.result.value), self.operator
+                )
+            )
+            self.operator = data
+            if self.result.value == "Error":
+                self.operand1 = 0
+            else:
+                self.operand1 = float(self.result.value)
+                self.new_operand = True
+
+        elif data in ("="):
+            self.result.value = str(
+                self.calculate(
+                    self.operand1, float(self.result.value), self.operator
+                )
+            )
+            self.reset()
+
+        elif data in ("%"):
+            self.result.value = str(float(self.result.value) / 100)
+            self.reset()
+
+        elif data in ("+/-"):
+            if float(self.result.value) > 0:
+                self.result.value = "-" + str(self.result.value)
+            elif float(self.result.value) < 0:
+                self.result.value = str(
+                    self.format_number(abs(float(self.result.value)))
+                )
+
+        self.update()
+
+    def format_number(self, num):
+        if num % 1 == 0:
+            return int(num)
+        else:
+            return num
+
+    def calculate(self, operand1, operand2, operator):
+        if operator == "+":
+            return self.format_number(operand1 + operand2)
+        elif operator == "-":
+            return self.format_number(operand1 - operand2)
+        elif operator == "*":
+            return self.format_number(operand1 * operand2)
+        elif operator == "/":
+            if operand2 == 0:
+                return "Error"
+            else:
+                return self.format_number(operand1 / operand2)
+
+    def reset(self):
+        self.operator = "+"
+        self.operand1 = 0
+        self.new_operand = True
+
 
 def main(page: ft.Page):
     page.title = "Calc App"
-    result = ft.Text(value="0")
-    page.add(
-        ft.Button("AC"), ft.Button("+/-"), ft.Button("%"), ft.Button("/"),
-        ft.Button("7"), ft.Button("8"), ft.Button("9"), ft.Button("*"),
-        ft.Button("4"), ft.Button("5"), ft.Button("6"), ft.Button("-"),
-        ft.Button("1"), ft.Button("2"), ft.Button("3"), ft.Button("+"),
-        ft.Button("0"), ft.Button("."), ft.Button("="),
-    )
+    # create application instance
+    calc = CalculatorApp()
+
+    # add application's root control to the page
+    page.add(calc)
+
 
 if __name__ == "__main__":
     ft.run(main)
-    )
-
-if __name__ == "__main__":
-    ft.run(main)
-
